f44bd4e2690d870557a7c1f175abef516e71bf21
其妙 <hling51325@163.com>
2021-05-14 10:36:05 +0800

    init

---
+++ src/components/GiftCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div :style="{ border: `solid 0.5px ${priceProperties.backgroundBottomColor}` }" class="message-super-chat">
+    <div :style="{
+        background: `${priceProperties.backgroundColor}`,
+        padding: '10px',
+      }">
+      <div :style="{ display: 'inline-block', 'vertical-align': 'top' }">
+        <Avatar :src="avatar" size="large" />
+      </div>
+      <div :style="{ display: 'inline-block' }">
+        <p>{{ name }}</p>
+        <template v-if="isGuardGift">
+          <p>
+            {{ giftNumber === 1 ? `${giftName}` : `${giftName}×${giftNumber}` }}
+          </p>
+        </template>
+        <template v-else-if="totalPrice">
+          <p>{{ `￥${totalPrice}` }}</p>
+        </template>
+      </div>
+    </div>
+    <div :style="{
+        background: `${priceProperties.backgroundBottomColor}`,
+        color: 'white',
+      }">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    "priceProperties",
+    "avatar",
+    "name",
+    "giftName",
+    "totalPrice",
+    "giftNumber",
+    "isGuardGift",
+  ],
+};
+</script>
+
+<style scoped>
+.message-super-chat {
+  border-radius: 10px;
+  border: solid 0.5px rgba(66, 125, 158, 1);
+  margin: 5px;
+  overflow: hidden;
+}
+</style>
